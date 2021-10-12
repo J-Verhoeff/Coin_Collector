bb4a95c01463d8d1749cb6ebc51ec9e0cfe45ae7
Josh Verhoeff <jverhoeff98@gmail.com>
2021-10-11 22:10:47 -0400

    Renamed project

---
+++ Coin_Collector/Assets/Scripts/SideToSideMovement.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SideToSideMovement : MonoBehaviour {
+    [SerializeField] float movementSpeed = 10f;
+
+    void Update() {
+        float xDirection = Input.GetAxis("Horizontal");
+
+        transform.Translate(Vector3.right * (xDirection * movementSpeed * Time.deltaTime));
+    }
+}
